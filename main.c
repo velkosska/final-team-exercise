d18c9f163f793cb2a79fbfba0afc2c4af0350291
Marija <velkosska@gmail.com>
2025-04-30 11:49:27 +0200

    Use this version please :D

--- main.c
+++ main.c
@@ -2,21 +2,18 @@
 #include <string.h>
 #include "media_manager.h"
 
-// Main function to launch the Media Database Manager application
 int main(int argc, char *argv[]) {
-    // Default filename for the media database
     char filename[MAX_STRING] = "full_media_list.csv";
 
-    // Allow the user to provide a filename as a command-line argument
+    // Allow user to set filename via command-line or prompt
     if (argc > 1) {
         strncpy(filename, argv[1], MAX_STRING);
         filename[MAX_STRING - 1] = '\0';
     } else {
-        // Ask the user to input a file name or press Enter to use the default
         char temp[MAX_STRING];
         printf("Enter CSV file name (or press Enter for default '%s'): ", filename);
         if (fgets(temp, MAX_STRING, stdin)) {
-            strtok(temp, "\n"); // Remove trailing newline
+            strtok(temp, "\n");
             if (validateInput(temp)) {
                 strncpy(filename, temp, MAX_STRING);
                 filename[MAX_STRING - 1] = '\0';
@@ -24,41 +21,50 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Display main menu options
-    int mode;
-    printf("\nWelcome to the Media Database Manager\n");
-    printf("Select mode:\n");
-    printf("1. Saving Mode\n");
-    printf("2. Reading Mode\n");
-    printf("Enter your choice: ");
-    scanf("%d", &mode);
-    getchar();  // Clear leftover newline
-
-    if (mode == 1) {
-        // Submenu for saving-related operations
-        int submode;
-        printf("\nSaving Mode:\n");
-        printf("1. Create New Entry\n");
-        printf("2. Edit Existing Entry\n");
-        printf("3. Delete Entry\n"); 
+    while (1) {
+        int mode;
+        printf("\n=== Media Database Manager ===\n");
+        printf("1. Saving Mode (Create, Edit, Delete)\n");
+        printf("2. Reading Mode (Search & View)\n");
+        printf("3. Exit\n");
         printf("Enter your choice: ");
-        scanf("%d", &submode);
-        getchar();  // Clear leftover newline
-
-        if (submode == 1) {
-            saveEntry(filename);
-        } else if (submode == 2) {
-            editEntry(filename);
-        } else if (submode == 3) {
-            deleteEntry(filename);
-        } else {
-            printf("Invalid choice. Exiting.\n");
+        if (scanf("%d", &mode) != 1) {
+            printf("Invalid input. Exiting.\n");
+            break;
         }
-    } else if (mode == 2) {
-        // Start reading/searching media entries
-        readEntries(filename);
-    } else {
-        printf("Invalid mode. Exiting.\n");
+        getchar();  // Clear newline
+
+        if (mode == 1) {
+            int submode;
+            printf("\n--- Saving Mode ---\n");
+            printf("1. Create New Entry\n");
+            printf("2. Edit Existing Entry\n");
+            printf("3. Delete Entry\n");
+            printf("4. Return to Main Menu\n");
+            printf("Enter your choice: ");
+            scanf("%d", &submode);
+            getchar();
+
+            if (submode == 1) {
+                saveEntry(filename);
+            } else if (submode == 2) {
+                editEntry(filename);
+            } else if (submode == 3) {
+                deleteEntry(filename);
+            } else if (submode == 4) {
+                continue;  // Return to main menu
+            } else {
+                printf("Invalid choice. Returning to main menu.\n");
+            }
+
+        } else if (mode == 2) {
+            readEntries(filename);
+        } else if (mode == 3) {
+            printf("Exiting program. Goodbye!\n");
+            break;
+        } else {
+            printf("Invalid mode. Try again.\n");
+        }
     }
 
     return 0;
